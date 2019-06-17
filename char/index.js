f2844f03748b2a23e01037d16c177a2c7f25525c
guozhuanghu <guozhuanghu@163.com>
2019-06-17 15:50:29 +0800

    replace exports method

--- char/index.js
+++ char/index.js
@@ -3,11 +3,10 @@ var array = require("./lib/array");
 var date = require("./lib/date");
 var val = require("./lib/val");
 var obj = require("./lib/obj");
-var htexports = {
+module.exports = {
     char: char,
     array: array,
     date: date,
     val: val,
     obj: obj
 };
-module.exports = htexports;
